efc82d0ec87b4a49f8d55788d9b9e3d349f85e7e
Yeuyo <38599824+Yeuyo@users.noreply.github.com>
2020-04-20 16:35:54 +0100

    Update LinearHexahedral.m
    Modified to fit the thesis margin without line break.

--- Hexahedral/Linear/LinearHexahedral.m
+++ Hexahedral/Linear/LinearHexahedral.m
@@ -1,8 +1,9 @@
-clear; [E,v,coord,etopol,bc,f]=cantilever_endload;
-nodes=size(coord,1); nDoF=3*nodes; nels=size(etopol,1); eDoF=zeros(24,1);
-DoF=reshape(1:nDoF,3,nodes)'; K=zeros(nDoF); d=zeros(nDoF,1); Gpsig=[];
-Iz=zeros(6); Iz(1:3,1:3)=ones(3); I2=eye(6); I2(4:6,4:6)=0.5*eye(3);
-D=E/((1+v)*(1-2*v))*(v*Iz+((1-2*v)*I2)); wp=ones(8,1); dN=zeros(24,8);
+[E,v,coord,etopol,bc,f]=cantilever_endload;
+nodes=size(coord,1); nDoF=3*nodes; nels=size(etopol,1);
+eDoF=zeros(24,1); DoF=reshape(1:nDoF,3,nodes)'; K=zeros(nDoF);
+uvw=zeros(nDoF,1); Gpsig=[]; Iz=zeros(6); Iz(1:3,1:3)=ones(3);
+I2=eye(6); I2(4:6,4:6)=0.5*eye(3); wp=ones(8,1); dN=zeros(24,8);
+D=E/((1+v)*(1-2*v))*(v*Iz+((1-2*v)*I2));
 Gpcoord=zeros(nels,8,3); BeGp=zeros(nels,8,6,24);
 xsi=[-1 -1  1  1 -1 -1  1  1]'/sqrt(3);
 eta=[-1 -1 -1 -1  1  1  1  1]'/sqrt(3);
@@ -22,14 +23,15 @@ for nel=1:nels
   JT=dN*coord(etopol(nel,:),:); ke=zeros(24);
   for i=1:8; indx=[(3*i)-2:3*i]';
     Gpcoord(nel,i,1:3)=N(i,:)*coord(etopol(nel,:),1:3);
-    detJ=det(JT(indx,:)); dNx=(JT(indx,:))\dN(indx,:); B=zeros(6,24);
-    B(1,1:3:end)=dNx(1,:); B(2,2:3:end)=dNx(2,:); B(3,3:3:end)=dNx(3,:);
-    B(4,1:3:end)=dNx(2,:); B(4,2:3:end)=dNx(1,:); B(5,2:3:end)=dNx(3,:);
-    B(5,3:3:end)=dNx(2,:); B(6,1:3:end)=dNx(3,:); B(6,3:3:end)=dNx(1,:);
-    BeGp(nel,i,:,:)=B;
+    detJ=det(JT(indx,:)); dNx=(JT(indx,:))\dN(indx,:);
+    B=zeros(6,24); B(1,1:3:end)=dNx(1,:); B(2,2:3:end)=dNx(2,:);
+    B(3,3:3:end)=dNx(3,:); B(4,1:3:end)=dNx(2,:);
+    B(4,2:3:end)=dNx(1,:); B(5,2:3:end)=dNx(3,:);
+    B(5,3:3:end)=dNx(2,:); B(6,1:3:end)=dNx(3,:);
+    B(6,3:3:end)=dNx(1,:); BeGp(nel,i,:,:)=B;
     ke=ke+(B'*D*B*detJ*wp(i));
   end
   K(eDoF,eDoF)=K(eDoF,eDoF)+ke;
 end
 fDoF=[1:nDoF]'; pDoF=bc(:,1); d(pDoF)=bc(:,2); fDoF(pDoF)=[];
-d(fDoF)=K(fDoF,fDoF)\(f(fDoF)-K(fDoF,pDoF)*d(pDoF));
+uvw(fDoF)=K(fDoF,fDoF)\(f(fDoF)-K(fDoF,pDoF)*d(pDoF));
